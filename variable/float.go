a6ef163dddeb405029ef53f343312de56daa540e
tomato <huangyuxine@gmail.com>
2024-07-15 15:46:55 +0800

    float

--- variable/float.go
+++ variable/float.go
@@ -1,1 +1,16 @@
 package main
+
+import "fmt"
+
+func main() {
+	// 3.7E+1 表示 37
+	// 3.7E-2 表示 0.037
+
+	// 有效位7位, 8位开始损失精度
+	var num1 float32 = 100000291
+	fmt.Println(num1) // 1.0000029e+08
+
+	// 有效位15位, 16位就开始损失精度
+	var num2 float64 = 1000000000000092
+	fmt.Println(num2) // 1.000000000000092e+15
+}
